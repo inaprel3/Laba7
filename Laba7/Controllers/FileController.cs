937d19eb97796de8577e163efd3ff53ad94e5731
Nastya <staprost.22@gmail.com>
2024-03-17 20:21:28 +0100

    Добавьте файлы проекта.

---
+++ Laba7/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace Laba7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            string content = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(content);
+
+            return File(byteArray, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
